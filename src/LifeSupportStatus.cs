7672b22770bb586b0a2b558d966d496a770bd8ce
Steve Benz <s8878992@hotmail.com>
2018-12-23 15:57:14 -0800

    Jeb eats snacks and gets grumpy if there aren't any!

---
+++ src/LifeSupportStatus.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nerm.Colonization
+{
+    public class LifeSupportStatus
+    {
+        public string KerbalName;
+        public double LastMeal;
+        public bool IsGrouchy;
+        public string OldTrait;
+    }
+}
--- src/SnackConsumption.cs
+++ src/SnackConsumption.cs
@@ -12,6 +12,9 @@ namespace Nerm.Colonization
         [KSPField(isPersistant = true)]
         public double LastUpdateTime;
 
+        // Assumes that the time things come back as fractions of a second.
+        const float SupplyConsumptionPerDayPerKerbal = 1f / (6f * 60f * 60f);
+
         protected IResourceBroker _resBroker;
         public IResourceBroker ResBroker
         {
@@ -34,6 +37,14 @@ namespace Nerm.Colonization
                 return;
             }
 
+            // Compute this early, since it sets the last update time member
+            double deltaTime = GetDeltaTime();
+            if (deltaTime < 0)
+            {
+                // This is the first update - don't eat anything.
+                return;
+            }
+
             if (vessel.isEVA)
             {
                 // TODO: What to do here?  Should Kerbals on EVA ever go hungry?
@@ -47,28 +58,56 @@ namespace Nerm.Colonization
                 return;
             }
 
-            double deltaTime = GetDeltaTime();
-            ConversionRecipe conversionRecipe = new ConversionRecipe();
-            conversionRecipe.Inputs.Add(new ResourceRatio()
+            if (this.IsAtHome)
             {
-                FlowMode = ResourceFlowMode.ALL_VESSEL,
-                Ratio = 1.0 * crew.Count,
-                ResourceName = "Snacks",
-                DumpExcess = true
-            });
-            conversionRecipe.Outputs.Add(new ResourceRatio()
+                // While actually on Kerbal, the Kerbals will order take-out rather than consuming
+                // what's in the ship.
+                foreach (var crewman in crew)
+                {
+                    LifeSupportScenario.Instance?.KerbalHasReachedHomeworld(crewman);
+                }
+            }
+            else
             {
-                FlowMode = ResourceFlowMode.ALL_VESSEL,
-                Ratio = 1.0,
-                ResourceName = "Stinkies",
-                DumpExcess = true
-            });
-            var crewPart = vessel.parts.FirstOrDefault(p => p.CrewCapacity > 0);
-            ConverterResults result = this.ResConverter.ProcessRecipe(deltaTime, conversionRecipe, crewPart, null, 1f);
-            Debug.Log($"deltaTime={deltaTime} result.TimeFactor={result.TimeFactor} result.Status='{result.Status}'");
+                ConversionRecipe conversionRecipe = new ConversionRecipe();
+                conversionRecipe.Inputs.Add(new ResourceRatio()
+                {
+                    FlowMode = ResourceFlowMode.ALL_VESSEL,
+                    Ratio = SupplyConsumptionPerDayPerKerbal * crew.Count,
+                    ResourceName = "Snacks",
+                    DumpExcess = true
+                });
+                conversionRecipe.Outputs.Add(new ResourceRatio()
+                {
+                    FlowMode = ResourceFlowMode.ALL_VESSEL,
+                    Ratio = SupplyConsumptionPerDayPerKerbal * crew.Count,
+                    ResourceName = "Stinkies",
+                    DumpExcess = true
+                });
+                var crewPart = vessel.parts.FirstOrDefault(p => p.CrewCapacity > 0);
+                ConverterResults result = this.ResConverter.ProcessRecipe(deltaTime, conversionRecipe, crewPart, null, 1f);
+
+                // TODO: result.TimeFactor should be used as the value for 'lastMeal', which should be passed in.
+                //  Then you can call missed-a-meal.
+
+                bool gotAMeal = result.TimeFactor >= deltaTime - ResourceUtilities.FLOAT_TOLERANCE;
+                foreach (var crewman in crew)
+                {
+                    if (gotAMeal)
+                    {
+                        LifeSupportScenario.Instance?.KerbalHadASnack(crewman);
+                    }
+                    else
+                    {
+                        LifeSupportScenario.Instance?.KerbalMissedAMeal(crewman);
+                    }
+                }
+            }
         }
 
-        protected double GetDeltaTime()
+        private bool IsAtHome => vessel.mainBody == FlightGlobals.GetHomeBody() && vessel.altitude < 10000;
+
+        private double GetDeltaTime()
         {
             if (Time.timeSinceLevelLoad < 1.0f || !FlightGlobals.ready)
             {
@@ -88,6 +127,5 @@ namespace Nerm.Colonization
             LastUpdateTime += deltaTime;
             return deltaTime;
         }
-
     }
 }
