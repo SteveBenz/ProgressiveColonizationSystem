69822ea0d5b6d0c7629895ef8095c898cf7f240e
Steve Benz <s8878992@hotmail.com>
2019-01-17 21:33:33 -0800

    Broke out the placement and persistence bits of the dialog thing, and created a dialog for the editors

---
+++ src/LifeSupportCalculator.cs
@@ -0,0 +1,28 @@
+﻿using KSP.UI.Screens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+// TODO: Boy Voyage has a less bad way of doing positioning:
+//   https://github.com/jarosm/KSP-BonVoyage/blob/master/BonVoyage/gui/MainWindowView.cs
+
+namespace Nerm.Colonization
+{
+    /// <summary>
+    ///   This class maintains a toolbar button and a GUI display that allows the user to see
+    ///   into the life support status of the active vessel.
+    /// </summary>
+    [KSPScenario(ScenarioCreationOptions.AddToAllGames, GameScenes.EDITOR)]
+    public class LifeSupportCalculator
+        : CbnToolbarDialog
+    {
+        protected override MultiOptionDialog DrawDialog()
+        {
+            return new MultiOptionDialog("LifeSupportCalculator", "howyadoin", "Life Support Calculator", HighLogic.UISkin, new DialogGUIVerticalLayout());
+        }
+
+        protected override ApplicationLauncher.AppScenes VisibleInScenes { get; } = ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.VAB;
+    }
+}
--- src/LifeSupportStatusMonitor.cs
+++ src/LifeSupportStatusMonitor.cs
@@ -16,154 +16,66 @@ namespace Nerm.Colonization
     /// </summary>
     [KSPScenario(ScenarioCreationOptions.AddToAllGames, GameScenes.FLIGHT)]
     public class LifeSupportStatusMonitor
-        : ScenarioModule
+        : CbnToolbarDialog
     {
-        [KSPField(isPersistant = true)]
-        public bool isVisible = false;
-
         // If simulating + or - crewman, this becomes positive or negative.
         private int crewDelta = 0;
-
         // CrewDelta gets reset when lastActiveVessel no longer equals the current vessel.
         private Vessel lastActiveVessel;
-
-        private ApplicationLauncherButton toolbarButton;
-
-        private PopupDialog dialog = null;
         private string consumptionAndProductionInformation;
-
-        [KSPField(isPersistant = true)]
-        public float x;
-        [KSPField(isPersistant = true)]
-        public float y;
-
-        private bool toolbarStateMatchedToIsVisible;
-
         private bool showingWhatIfButtons;
         private bool showingResourceTransfer;
 
         private IntervesselResourceTransfer resourceTransfer = new IntervesselResourceTransfer();
 
-        public override void OnAwake()
-        {
-            base.OnAwake();
+        protected override bool IsRelevant => FlightGlobals.ActiveVessel.GetCrewCount() > 0 && !FlightGlobals.ActiveVessel.isEVA;
+        protected override ApplicationLauncher.AppScenes VisibleInScenes { get; } = ApplicationLauncher.AppScenes.FLIGHT;
 
-            AttachToToolbar();
-        }
-
-        private void AttachToToolbar()
+        protected override MultiOptionDialog DrawDialog()
         {
-            if (this.toolbarButton != null)
+            // FYI, if you want to override a style, here'd be a way to do it:
+            // var myStyle = new UIStyle(UISkinManager.defaultSkin.label) { wordWrap = false};
+            //
+            // Too bad wordWrap doesn't get paid attention to.
+
+            List<DialogGUIBase> parts = new List<DialogGUIBase>();
+            parts.Add(new DialogGUILabel(() => this.consumptionAndProductionInformation));
+            parts.Add(new DialogGUIFlexibleSpace());
+            if (showingWhatIfButtons)
             {
-                // defensive
-                return;
+                parts.Add(new DialogGUIHorizontalLayout(TextAnchor.MiddleLeft,
+                            new DialogGUILabel("What if we"),
+                            new DialogGUIButton("Add", () => { ++crewDelta; }, () => true, false),
+                            new DialogGUILabel("/"),
+                            new DialogGUIButton("Remove", () => { --crewDelta; }, () => FlightGlobals.ActiveVessel.GetCrewCount() + this.crewDelta > 1, false),
+                            new DialogGUILabel("a kerbal?")));
             }
 
-            Texture2D appLauncherTexture = new Texture2D(36, 36, TextureFormat.ARGB32, false);
-            appLauncherTexture.LoadImage(Properties.Resources.AppLauncherIcon);
-
-            Debug.Assert(ApplicationLauncher.Ready, "ApplicationLauncher is not ready - can't add the toolbar button.  Is this possible, really?  If so maybe we could do it later?");
-            this.toolbarButton = ApplicationLauncher.Instance.AddModApplication(ShowDialog, HideDialog, null, null, null, null,
-                ApplicationLauncher.AppScenes.FLIGHT, appLauncherTexture);
-            this.toolbarStateMatchedToIsVisible = false;
-        }
-
-        private void ShowDialog()
-        {
-            isVisible = true;
-            if (this.dialog == null)
+            if (showingResourceTransfer)
             {
-                if (this.consumptionAndProductionInformation == null)
-                {
-                    // Fixed update hasn't run yet.
-                    return;
-                }
+                parts.Add(
+                    new DialogGUIVerticalLayout(
+                        new DialogGUILabel("<color #c0c0c0>______________________________________________</color>"),
+                        new DialogGUILabel("<b>Resource Transfer</b>"),
+                        new DialogGUIHorizontalLayout(TextAnchor.MiddleLeft,
+                            new DialogGUILabel("Target: "),
+                            new DialogGUILabel(resourceTransfer.TargetVessel?.GetDisplayName())),
+                        new DialogGUIHorizontalLayout(TextAnchor.MiddleLeft,
+                            new DialogGUIButton("Start", resourceTransfer.StartTransfer, () => resourceTransfer.TargetVessel != null && !resourceTransfer.IsTransferUnderway, dismissOnSelect: false),
+                            new DialogGUISlider(() => (float)resourceTransfer.TransferPercent, 0, 1, false, 100, 20, null))));
+            }
 
-                // FYI, if you want to override a style, here'd be a way to do it:
-                // var myStyle = new UIStyle(UISkinManager.defaultSkin.label) { wordWrap = false};
-                //
-                // Too bad wordWrap doesn't get paid attention to.
-
-                List<DialogGUIBase> parts = new List<DialogGUIBase>();
-                parts.Add(new DialogGUILabel(() => this.consumptionAndProductionInformation));
-                parts.Add(new DialogGUIFlexibleSpace());
-                if (showingWhatIfButtons)
-                {
-                    parts.Add(new DialogGUIHorizontalLayout(TextAnchor.MiddleLeft,
-                                new DialogGUILabel("What if we"),
-                                new DialogGUIButton("Add", () => { ++crewDelta; }, () => true, false),
-                                new DialogGUILabel("/"),
-                                new DialogGUIButton("Remove", () => { --crewDelta; }, () => FlightGlobals.ActiveVessel.GetCrewCount() + this.crewDelta > 1, false),
-                                new DialogGUILabel("a kerbal?")));
-                }
-
-                if (showingResourceTransfer)
-                {
-                    parts.Add(
-                        new DialogGUIVerticalLayout(
-                            new DialogGUILabel("<color #c0c0c0>______________________________________________</color>"),
-                            new DialogGUILabel("<b>Resource Transfer</b>"),
-                            new DialogGUIHorizontalLayout(TextAnchor.MiddleLeft,
-                                new DialogGUILabel("Target: "),
-                                new DialogGUILabel(resourceTransfer.TargetVessel?.GetDisplayName())),
-                            new DialogGUIHorizontalLayout(TextAnchor.MiddleLeft,
-                                new DialogGUIButton("Start", resourceTransfer.StartTransfer, () => resourceTransfer.TargetVessel != null && !resourceTransfer.IsTransferUnderway, dismissOnSelect: false),
-                                new DialogGUISlider(() => (float)resourceTransfer.TransferPercent, 0, 1, false, 100, 20, null))));
-                }
-
-                this.dialog = PopupDialog.SpawnPopupDialog(
-                    new Vector2(.5f, .5f),
-                    new Vector2(.5f, .5f),
-                    new MultiOptionDialog(
+            return new MultiOptionDialog(
                         "LifeSupportMonitor",  // <- no idea what this does.
                         "",
                         "Colony Status",
                         HighLogic.UISkin,
-                        new DialogGUIVerticalLayout(parts.ToArray())),
-                    persistAcrossScenes: false,
-                    skin: HighLogic.UISkin,
-                    isModal: false,
-                    titleExtra: "TITLE EXTRA!"); // <- no idea what that does.
-            }
+                        new DialogGUIVerticalLayout(parts.ToArray()));
         }
 
-        private void HideDialog()
-        {
-            isVisible = false;
-            this.dialog?.Dismiss();
-            this.dialog = null;
-            this.crewDelta = 0;
-        }
-
-        private void OnDestroy()
-        {
-            if (this.toolbarButton != null)
-            {
-                if (ApplicationLauncher.Instance != null)
-                {
-                    ApplicationLauncher.Instance.RemoveModApplication(this.toolbarButton);
-                }
-                this.toolbarButton = null;
-            }
-        }
-
-        private void FixedUpdate()
+        protected override void OnFixedUpdate()
         {
             resourceTransfer.OnFixedUpdate();
-
-            if (FlightGlobals.ActiveVessel.GetCrewCount() == 0 || FlightGlobals.ActiveVessel.isEVA)
-            {
-                if (this.dialog != null)
-                {
-                    this.dialog.Dismiss();
-                    this.dialog = null;
-                    // But leave isVisible set, as that's the persistent view.
-                }
-                this.toolbarButton.Disable();
-                return;
-            }
-
-            this.toolbarButton.Enable();
 
             if (this.lastActiveVessel != FlightGlobals.ActiveVessel)
             {
@@ -178,54 +90,17 @@ namespace Nerm.Colonization
                 return;
             }
 
-            // Shenanigans!  This gets around the apparent fact that you can't tell the toolbar what state to start in.
-            if (!this.toolbarStateMatchedToIsVisible)
-            {
-                this.toolbarButton.toggleButton.Value = this.isVisible;
-                this.toolbarStateMatchedToIsVisible = true;
-            }
-
-            // Shenanigans!  This hack gets around the apparent fact that you can't tell the window where to position itself.
-            // Unity Shenanigans!  this.dialog?.dialog?.popupwindow can throw a null reference exception...  huh?
-            if (this.dialog != null && this.dialog.popupWindow?.transform?.localPosition != null)
-            {
-                if ((x > 1f || y > 1f || x < 1f || y < 1f)
-                    && this.dialog.popupWindow.transform.localPosition.x == 0 && this.dialog.popupWindow.transform.localPosition.y == 0)
-                {
-                    if (x > 1f || y > 1f || x < 1f || y < 1f)
-                    {
-                        // Re-apply the previous translation - adjusting for UI Scale
-                        this.dialog.popupWindow.transform.Translate(x * GameSettings.UI_SCALE, y * GameSettings.UI_SCALE, 0f);
-                        // If we have to persist this hack, we should detect whether the thing is pushed off the screen.
-                    }
-                }
-                else
-                {
-                    // Record the translation for the future.
-                    x = this.dialog.popupWindow.transform.localPosition.x;
-                    y = this.dialog.popupWindow.transform.localPosition.y;
-                }
-            }
-
             int crewCount = FlightGlobals.ActiveVessel.GetCrewCount();
             if ((crewCount > 0) != this.showingWhatIfButtons)
             {
                 this.showingWhatIfButtons = (crewCount > 0);
-                if (this.dialog)
-                {
-                    this.dialog.Dismiss();
-                    this.dialog = null;
-                }
+                this.ForceRebuild();
             }
 
             if ((resourceTransfer.TargetVessel != null) != this.showingResourceTransfer)
             {
                 this.showingResourceTransfer = (resourceTransfer.TargetVessel != null);
-                if (this.dialog)
-                {
-                    this.dialog.Dismiss();
-                    this.dialog = null;
-                }
+                this.ForceRebuild();
             }
 
             activeSnackConsumption.ResourceQuantities(out var availableResources, out var availableStorage);
@@ -233,15 +108,7 @@ namespace Nerm.Colonization
 
             BuildStatusString(activeSnackConsumption, availableResources, availableStorage, snackProducers, crewCount, crewDelta, out string message);
             this.consumptionAndProductionInformation = message;
-
-            if (this.isVisible && this.dialog == null)
-            {
-                this.ShowDialog();
-            }
         }
-
-
-
 
         internal static void BuildStatusString(
             SnackConsumption activeSnackConsumption,
