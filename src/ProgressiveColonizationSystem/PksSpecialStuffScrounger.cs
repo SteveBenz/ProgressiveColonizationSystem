9bd0a655180e7a21fff97e09cc36e6001dede61c
Steve Benz <s8878992@hotmail.com>
2019-02-23 21:10:16 -0800

    Added a part for scrounging

---
+++ src/ProgressiveColonizationSystem/PksSpecialStuffScrounger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProgressiveColonizationSystem
+{
+    public class PksSpecialStuffScrounger
+        : PksTieredResourceConverter
+    {
+        [KSPEvent(guiActive = true)]
+        public void FindResource()
+        {
+        }
+    }
+}
