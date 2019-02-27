e70d2fff8ffe7b39ca6eca6ea62c8766db68defd
Steve Benz <s8878992@hotmail.com>
2019-02-26 22:12:44 -0800

    Create waypoints without dependency on WaypointManager

--- src/ProgressiveColonizationSystem/PksSpecialStuffScrounger.cs
+++ src/ProgressiveColonizationSystem/PksSpecialStuffScrounger.cs
@@ -25,7 +25,8 @@ namespace ProgressiveColonizationSystem
                 Waypoint waypoint = FinePrint.WaypointManager.Instance().Waypoints.FirstOrDefault(wp => wp.navigationId == myId);
                 if (waypoint != null)
                 {
-                    global::WaypointManager.CustomWaypoints.RemoveWaypoint(waypoint);
+                    ScenarioCustomWaypoints.RemoveWaypoint(waypoint);
+                    //global::WaypointManager.CustomWaypoints.RemoveWaypoint(waypoint);
                 }
             }
 
@@ -40,9 +41,12 @@ namespace ProgressiveColonizationSystem
             waypoint.id = "custom";
             waypoint.seed = 269;
 
-            waypoint.height = global::WaypointManager.Util.WaypointHeight(waypoint, FlightGlobals.GetHomeBody());
+            waypoint.height = WaypointHeight(waypoint, FlightGlobals.GetHomeBody());
             waypoint.altitude = 0;
-            global::WaypointManager.CustomWaypoints.AddWaypoint(waypoint);
+
+            ScenarioCustomWaypoints.AddWaypoint(waypoint);
+
+            //global::WaypointManager.CustomWaypoints.AddWaypoint(waypoint);
             this.lastId = waypoint.navigationId.ToString();
 
             //waypoint.RandomizeNear(latitude, longitude, 100000);
@@ -57,6 +61,27 @@ namespace ProgressiveColonizationSystem
 
             //navWaypoint.Setup(waypoint);
             //navWaypoint.Activate();
+        }
+
+
+        public static double WaypointHeight(Waypoint w, CelestialBody body)
+        {
+            return TerrainHeight(w.latitude, w.longitude, body);
+        }
+
+        public static double TerrainHeight(double latitude, double longitude, CelestialBody body)
+        {
+            // Not sure when this happens - for Sun and Jool?
+            if (body.pqsController == null)
+            {
+                return 0;
+            }
+
+            // Figure out the terrain height
+            double latRads = Math.PI / 180.0 * latitude;
+            double lonRads = Math.PI / 180.0 * longitude;
+            Vector3d radialVector = new Vector3d(Math.Cos(latRads) * Math.Cos(lonRads), Math.Sin(latRads), Math.Cos(latRads) * Math.Sin(lonRads));
+            return Math.Max(body.pqsController.GetSurfaceHeight(radialVector) - body.pqsController.radius, 0.0);
         }
     }
 }
