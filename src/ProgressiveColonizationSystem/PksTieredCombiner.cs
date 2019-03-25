3972876e24be210343056c3af13125cac054170f
Steve Benz <s8878992@hotmail.com>
2019-03-24 22:20:51 -0700

    Made the first part - but it looks like a rethink of how the parts are made will be necessary to have a GetInfo() that does anything useful

---
+++ src/ProgressiveColonizationSystem/PksTieredCombiner.cs
@@ -0,0 +1,46 @@
+﻿namespace ProgressiveColonizationSystem
+{
+    public class PksTieredCombiner
+        : PartModule, ITieredCombiner
+    {
+        [KSPField]
+        public string output;
+
+        [KSPField]
+        public string tieredInput;
+
+        [KSPField]
+        public string untieredInput;
+
+        [KSPField]
+        public float capacity;
+
+        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Research")]
+        public string researchStatus;
+
+        private static readonly double[] combinationRates = { .4, .65, .8, .92, .98 };
+
+        private TieredResource tieredInputAsTieredResource = null;
+
+        double ITieredCombiner.ProductionRate => this.capacity;
+
+        string ITieredCombiner.NonTieredOutputResourceName => this.output;
+
+        TieredResource ITieredCombiner.TieredInput
+        {
+            get
+            {
+                if (this.tieredInputAsTieredResource == null && !string.IsNullOrEmpty(this.tieredInput))
+                {
+                    this.tieredInputAsTieredResource = ColonizationResearchScenario.GetTieredResourceByName(this.tieredInput);
+                }
+                return this.tieredInputAsTieredResource;
+            }
+        }
+
+        double ITieredCombiner.GetRatioForTier(TechTier tier)
+            => combinationRates[(int)tier];
+
+        string ITieredCombiner.NonTieredInputResourceName => this.untieredInput;
+    }
+}
