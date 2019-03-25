e7273c1dcfa8bb0a5d0bf32bbb860b6e674f6e1a
Steve Benz <s8878992@hotmail.com>
2019-03-24 22:34:59 -0700

    Part seems to work -- but needs resource definitions

--- src/ProgressiveColonizationSystem/PksTieredCombiner.cs
+++ src/ProgressiveColonizationSystem/PksTieredCombiner.cs
@@ -1,46 +1,49 @@
-﻿namespace ProgressiveColonizationSystem
+﻿using System.Text;
+
+namespace ProgressiveColonizationSystem
 {
     public class PksTieredCombiner
-        : PartModule, ITieredCombiner
+        : PksTieredResourceConverter, ITieredCombiner
     {
         [KSPField]
-        public string output;
-
-        [KSPField]
-        public string tieredInput;
+        public string untieredOutput;
 
         [KSPField]
         public string untieredInput;
 
-        [KSPField]
-        public float capacity;
-
-        [KSPField(guiActive = true, guiActiveEditor = false, guiName = "Research")]
-        public string researchStatus;
-
         private static readonly double[] combinationRates = { .4, .65, .8, .92, .98 };
-
-        private TieredResource tieredInputAsTieredResource = null;
 
         double ITieredCombiner.ProductionRate => this.capacity;
 
-        string ITieredCombiner.NonTieredOutputResourceName => this.output;
+        string ITieredCombiner.NonTieredOutputResourceName => this.untieredOutput;
 
-        TieredResource ITieredCombiner.TieredInput
-        {
-            get
-            {
-                if (this.tieredInputAsTieredResource == null && !string.IsNullOrEmpty(this.tieredInput))
-                {
-                    this.tieredInputAsTieredResource = ColonizationResearchScenario.GetTieredResourceByName(this.tieredInput);
-                }
-                return this.tieredInputAsTieredResource;
-            }
-        }
+        TieredResource ITieredCombiner.TieredInput => base.Output;
 
         double ITieredCombiner.GetRatioForTier(TechTier tier)
             => combinationRates[(int)tier];
 
         string ITieredCombiner.NonTieredInputResourceName => this.untieredInput;
+
+
+        public override string GetInfo()
+        {
+            StringBuilder info = new StringBuilder();
+
+            if (this.Input != null)
+            {
+                info.AppendLine($"{GreenInfo("Input:")} {this.Input.BaseName}");
+            }
+
+            info.AppendLine($"{GreenInfo("Capacity:")} {this.capacity} {this.Output.CapacityUnits}");
+            info.AppendLine($"{GreenInfo("Output:")} {this.untieredOutput}");
+
+            info.AppendLine($"{GreenInfo("%Local:")}");
+            foreach (TechTier tier in TechTierExtensions.AllTiers)
+            {
+                info.AppendLine($" {tier.ToString()}: {(int)(combinationRates[(int)tier] * 100)}%");
+            }
+
+            return info.ToString();
+        }
     }
 }
